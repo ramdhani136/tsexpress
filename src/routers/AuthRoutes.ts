2b311ba1d6093ffa42844ebdc87102c9e1ef7c0f
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2022-09-29 09:56:39 +0700

    update

--- src/routers/AuthRoutes.ts
+++ src/routers/AuthRoutes.ts
@@ -1,9 +1,10 @@
 import BaseRoutes from "./BaseRouter";
 import AuthController from "../controllers/AuthController";
+import validate from "../middleware/AuthValidator"; // middleware
 
 class AuthRoutes  extends BaseRoutes{
     public  routes(): void {
-        this.router.post("/register",AuthController.register);
+        this.router.post("/register",validate, AuthController.register);
         this.router.post("/login",AuthController.login);
     }
     
