a79bb7ddd7e2ae43f0e358bbe57ed88aa89b76b8
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2022-09-29 10:01:36 +0700

    update

--- src/middleware/AuthValidator.ts
+++ src/middleware/AuthValidator.ts
@@ -1,18 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 import { check, validationResult } from "express-validator";
 
-const validate = [
-    check('username').isString(),
-    check('password').isLength({min:6}),
-    (req:Request,res:Response,next:NextFunction)=>{
-        const errors = validationResult(req);
 
-        if(!errors.isEmpty()){
-            return res.status(422).send({errors:errors.array()});
-        }else{
-            next();
+class AuthValidator{
+    public static validate = [
+        check('username').isString(),
+        check('password').isLength({min:6}),
+        (req:Request,res:Response,next:NextFunction)=>{
+            const errors = validationResult(req);
+    
+            if(!errors.isEmpty()){
+                return res.status(422).send({errors:errors.array()});
+            }else{
+                next();
+            }
         }
-    }
-]
+    ]
+    
+}
 
-export default validate;
+export default AuthValidator.validate;
--- src/routers/AuthRoutes.ts
+++ src/routers/AuthRoutes.ts
@@ -1,10 +1,10 @@
 import BaseRoutes from "./BaseRouter";
 import AuthController from "../controllers/AuthController";
-import validate from "../middleware/AuthValidator"; // middleware
+import AuthValidator from "../middleware/AuthValidator"; // middleware
 
 class AuthRoutes  extends BaseRoutes{
     public  routes(): void {
-        this.router.post("/register",validate, AuthController.register);
+        this.router.post("/register",AuthValidator, AuthController.register);
         this.router.post("/login",AuthController.login);
     }
     
