bab341a2a096fa3e9f60f37b4514eb6ecda45872
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2022-09-29 12:11:41 +0700

    update

--- src/middleware/AuthMiddleware.ts
+++ src/middleware/AuthMiddleware.ts
@@ -1,10 +1,22 @@
 import { Request,Response,NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
 export const auth = (req:Request,res:Response,next:NextFunction):any=>{
-    let auth =true;
-    if(auth){
-        next();
-    }else{
-        return res.send("anaunthenticated");
+  if(!req.headers.authorization){
+    return res.status(401).send("Forbidden, invalid token");
+  }
+
+  let secretKey = process.env.JWT_SECRET_KEY || "secret";
+  const token: string = req.headers.authorization.split(" ")[1];
+  
+  try {
+    const credential:string | object = jwt.verify(token,secretKey);
+
+    if(credential){
+        req.app.locals.credential = credential;
+        return next();
     }
+  } catch (error) {
+   return res.send(error);
+  }
 }
